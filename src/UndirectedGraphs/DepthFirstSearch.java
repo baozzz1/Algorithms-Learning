d9b8cf207258d0b6e1b3854a2c0b76b8d4d08271
baozzz1 <baozzz1@outlook.com>
2018-10-24 16:18:54 +0800

    DepthFirstSearch

---
+++ src/UndirectedGraphs/DepthFirstSearch.java
@@ -0,0 +1,46 @@
+package UndirectedGraphs;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+//使用深度优先搜索标记与起点连通的所有顶点所需的时间与顶点的度数之和成正比
+public class DepthFirstSearch{
+	private boolean[] marked;
+	private int count;
+
+	public DepthFirstSearch(Graph G, int s) {
+		marked = new boolean[G.V()];
+		dfs(G, s);
+	}
+
+	private void dfs(Graph G, int v) {
+		marked[v] = true;
+		count++;
+		for (int w : G.adj(v))
+			if (!marked[w])
+				dfs(G, w);
+	}
+
+	public boolean marked(int w) {
+		return marked[w];
+	}
+
+	public int count() {
+		return count;
+	}
+
+	public static void main(String[] args) {
+		Graph G = new Graph(new In(args[0]));
+		int s = Integer.parseInt(args[1]);
+		DepthFirstSearch search = new DepthFirstSearch(G, s);
+
+		for (int v = 0; v < G.V(); v++)
+			if (search.marked(v))
+				StdOut.print(v + " ");
+		StdOut.println();
+
+		if (search.count() != G.V())
+			StdOut.print("NOT ");
+		StdOut.println("connected");
+	}
+}
--- src/UndirectedGraphs/GraphClient.java
+++ src/UndirectedGraphs/GraphClient.java
@@ -34,40 +34,40 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class GraphClient {
 
-    // maximum degree 
-    public static int maxDegree(Graph G) {
-        int max = 0;
-        for (int v = 0; v < G.V(); v++)
-            if (G.degree(v) > max)
-                max = G.degree(v);
-        return max;
-    }
+	// maximum degree
+	public static int maxDegree(Graph G) {
+		int max = 0;
+		for (int v = 0; v < G.V(); v++)
+			if (Graph.degree(G,v) > max)
+				max = Graph.degree(G,v);
+		return max;
+	}
 
-    // average degree
-    public static int avgDegree(Graph G) {
-        // each edge incident on two vertices
-        return 2 * G.E() / G.V();
-    }
+	// average degree
+	public static int avgDegree(Graph G) {
+		// each edge incident on two vertices
+		return 2 * G.E() / G.V();
+	}
 
-    // number of self-loops
-    public static int numberOfSelfLoops(Graph G) {
-        int count = 0;
-        for (int v = 0; v < G.V(); v++)
-            for (int w : G.adj(v))
-                if (v == w) count++;
-        return count/2;   // self loop appears in adjacency list twice
-    } 
+	// number of self-loops
+	public static int numberOfSelfLoops(Graph G) {
+		int count = 0;
+		for (int v = 0; v < G.V(); v++)
+			for (int w : G.adj(v))
+				if (v == w)
+					count++;
+		return count / 2; // self loop appears in adjacency list twice
+	}
 
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        Graph G = new Graph(in);
-        StdOut.println(G);
+	public static void main(String[] args) {
+		In in = new In(args[0]);
+		Graph G = new Graph(in);
+		StdOut.println(G);
 
+		StdOut.println("vertex of maximum degree = " + maxDegree(G));
+		StdOut.println("average degree           = " + avgDegree(G));
+		StdOut.println("number of self loops     = " + numberOfSelfLoops(G));
 
-        StdOut.println("vertex of maximum degree = " + maxDegree(G));
-        StdOut.println("average degree           = " + avgDegree(G));
-        StdOut.println("number of self loops     = " + numberOfSelfLoops(G));
-
-    }
+	}
 
 }
